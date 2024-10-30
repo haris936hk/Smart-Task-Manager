4eb4aaa7efdd6cc925d45db9b81e260d219c728d
FURQAN QURESHI <Furqanqureshi347@gmail.com>
2024-10-30 14:28:39 +0500

    Done

--- ManagerDashboard.php
+++ ManagerDashboard.php
@@ -1,1 +1,16 @@
-//php code here
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="ManagerDashboard.css">
+    <title>ManagerDashboard</title>
+</head>
+<body>
+    <div class="Full">
+        <div class="bar"></div>
+        <div class="side"></div>
+        <div class="stats"></div>
+    </div>
+</body>
+</html>
