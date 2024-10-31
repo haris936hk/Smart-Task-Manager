8990e9af1cb872e8f8e1c76b7781986864577370
haris936hk <hariskhan936.hk@gmail.com>
2024-10-31 19:43:44 +0500

    EmployeeDashboard

---
+++ ManagerTaskList.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manager Task List</title>
+    <link rel="stylesheet" href="ManagerTaskList.css">
+</head>
+<body>
+    <table>
+        <th class="column">Task Name</th>
+        <th>Category</th>
+        <th>Status</th>
+        <th>Deadline</th>
+        <th>Assigned Members</th>
+        <th>Notes</th>
+        <th>Actions</th>
+    </table>
+</body>
+</html>
