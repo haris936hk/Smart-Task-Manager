e908cbf247f30a2996313e8cee3f938c1b51ab67
FURQAN QURESHI <Furqanqureshi347@gmail.com>
2024-11-04 01:04:30 +0500

    EmployeeTaskList

--- Login.php
+++ Login.php
@@ -1,1 +1,21 @@
-//Code here
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div id="container">
+
+        <div id="img">
+            <img src="" alt="">
+        </div>
+
+        <div id= "details">
+            <form action=""></fo
+        </div>
+
+    </div>
+</body>
+</html>
